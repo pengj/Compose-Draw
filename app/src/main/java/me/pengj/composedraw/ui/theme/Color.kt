dd397a8d76f785ff7eea6d4555cd542375ee13df
peng <anything.j@gmail.com>
2021-10-05 15:24:32 +0100

    update the color to black

--- app/src/main/java/me/pengj/composedraw/ui/theme/Color.kt
+++ app/src/main/java/me/pengj/composedraw/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package me.pengj.composedraw.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Black800 = Color(0xFF424242)
+val Black900 = Color(0xFF212121)
+val Black = Color(0x00000000)
+val White = Color(0xFFFFFFFF)
